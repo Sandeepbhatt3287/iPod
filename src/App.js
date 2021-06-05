49169f8ea8c8fc949819b93b8c5ce2639d1bdf71
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-06-05 21:35:15 +0530

    deleting app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="app-header">
         <p>
         iPod App
         </p>
