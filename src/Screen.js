5b50e1dcaf30f9459a3bf36f11b503c8a0ca3f00
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-06-23 22:13:02 +0530

    updated screen file

--- src/Screen.js
+++ src/Screen.js
@@ -1,3 +1,19 @@
+
 import React from 'react';
 import Home from './Home';
 import Music from './Music';
+
+
+class Screen extends React.Component{
+    render(){
+        return(
+            <div className="inner-player">
+                 {this.props.activePage==='Home'?<Home activeItem={this.props.activeItem}/>:null}
+                 {this.props.activePage==='Music'?<Music activeItem={this.props.activeItem}/>:null}
+            </div>
+        )
+    }
+}
+
+
+export default Screen;
